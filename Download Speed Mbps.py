f734d35820354b2355efef041a3deda1388e6172
Fernando Rodrigues <85629970+Fernando7181@users.noreply.github.com>
2022-11-02 19:49:42 -0300

    Delete Download Speed Mbps.py

--- Download Speed Mbps.py
+++
@@ -1,3 +0,0 @@
-file_size = float(input("Whats the size of the file in MB: "))
-internet_speed = float(input("And what could it be the speed in Mbps: "))
-print("The time that will take to download the file will be: "+ str((file_size / internet_speed) * 60) )

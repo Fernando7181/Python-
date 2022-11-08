aceeab17a3d6bf5687c6b56838734cb78920a35a
Fernando Rodrigues <85629970+Fernando7181@users.noreply.github.com>
2022-11-08 09:10:02 -0300

    Add files via upload

---
+++ Trabalho AV2/taxes.py
@@ -0,0 +1,8 @@
+#This Programm will be used to finish off some university assigments for taxes AV2 exam 
+custo = float(input("How much did yotu pay for the product: "))
+taxa_imposto = float(input("Please, insert the amount of taxes: "))
+taxa_imposto = taxa_imposto /100
+soma_imposto = (custo * taxa_imposto) 
+
+print(f'The amount of taxes was: {soma_imposto}')
+print(f'The amount of it with the taxes was: {custo - soma_imposto}')

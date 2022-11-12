8be8a8fe0f96c49dcae9da25e3aee49f4759f96a
Fernando Rodrigues <85629970+Fernando7181@users.noreply.github.com>
2022-11-12 19:02:57 -0300

    Update Time manegment.py

--- Trabalho AV2/Time manegment.py
+++ Trabalho AV2/Time manegment.py
@@ -1,17 +1,16 @@
+
 def calculator():
- n1 = int(input("Please, input the first two numbers for the time: "))
- if n1 == float:
+ continuee = 0
+ while continuee < 2:      
+  n1 = int(input("Please, input the first two numbers for the time: "))
+  if n1 == float:
        print("Please, it has to be a whole number.")
- n2 = int(input("Inset the second number of the time: "))
- if n2 > 60:
+  n2 = int(input("Inset the second number of the time: "))
+  if n2 > 60:
       print("Error")
- if n1 > 12:
+  if n1 > 12:
        print(f'{n1 - 12}:{n2}' +  "pm")
- elif n1 < 12:
+  elif n1 < 12:
             print(f'{n1}:{n2}' + "am")    
- continuee = int(input("Do you wish to continue? 1 for yes\n 2 for no: "))
- while continuee == 1:
-     calculator()
-     if continuee >= 2:
-         break
+  continuee = int(input("Do you wish to continue? 1 for yes\n 2 for no: "))
 calculator()
